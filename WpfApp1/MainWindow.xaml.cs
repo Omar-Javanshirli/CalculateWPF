e41d8ea4b536ec164c2ad17e670c6b29fd3c9f81
Omer Cavanshirli <omrcvnsrli@hotmail.com>
2022-08-20 18:16:27 +0400

    new update

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,52 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            foreach (UIElement element in MainGrid.Children)
+            {
+                if (element is Button)
+                {
+                    ((Button)element).Click += Button_Click;
+                }
+            }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string numberOrOperand = (string)((Button)e.OriginalSource).Content;
+
+            if (numberOrOperand == "C")
+            {
+                TextLabel.Text = string.Empty;
+            }
+            else if (numberOrOperand == "=")
+            {
+                string value = new DataTable().Compute(TextLabel.Text, null).ToString();
+                TextLabel.Text = value;
+            }
+            else if (TextLabel.Text.Length < 20)
+                Operation(numberOrOperand);
+        }
+
+        private void Operation(string operand)
+        {
+            string[] operands = { "%", "+", "-", "*", "/", "√", "," };
+            if (TextLabel.Text.Length != 0 && (operand == "%" || operand == "+" || operand == "-" ||
+              operand == "*" || operand == "/" || operand == "√" || operand == ","))
+            {
+                string lastCharOfTextLabel = TextLabel.Text[TextLabel.Text.Length - 1].ToString();
+                int sum = 0;
+                foreach (var item in operands)
+                {
+                    if (item == lastCharOfTextLabel)
+                        sum += 1;
+                }
+                if (sum != 1)
+                    TextLabel.Text += operand;
+
+            }
+            else
+                TextLabel.Text += operand;
+        }
+
     }
 }
